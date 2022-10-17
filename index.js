9043fb741338b18254327499622e22b5a3ad3501
lphi0011 <lphi0011@student.monash.edu>
2022-10-17 15:44:30 +1100

    done a lot of work

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ let bronzeButtonRef = document.getElementById('bronze-btn');
 
 // initial graphs
 var total_map = "graphs/total_map.json";
-vegaEmbed("#map", total_map).then(function(result) {
+vegaEmbed("#map", total_map, {"actions": false}).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var hosts = "graphs/hosts.json";
-vegaEmbed("#hosts", hosts).then(function(result) {
+vegaEmbed("#hosts", hosts, {"actions": false}).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 var hosts_map = "graphs/map_hosts.json";
-vegaEmbed("#hosts-map", hosts_map).then(function(result) {
+vegaEmbed("#hosts-map", hosts_map, {"actions": false}).then(function(result) {
+    // Access the Vega view instance
+    //(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+var top10 = "graphs/top10_total.json";
+vegaEmbed("#top10", top10, {"actions": false}).then(function(result) {
     // Access the Vega view instance
     //(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
@@ -30,25 +35,25 @@ var silver_map = "graphs/silver_map.json";
 var bronze_map = "graphs/bronze_map.json";
 
 goldButtonRef.addEventListener('click', function() {
-    vegaEmbed("#map", gold_map).then(function(result) {
+    vegaEmbed("#map", gold_map, {"actions": false}).then(function(result) {
         // Access the Vega view instance
         //(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 });
 silverButtonRef.addEventListener('click', function() {
-    vegaEmbed("#map", silver_map).then(function(result) {
+    vegaEmbed("#map", silver_map, {"actions": false}).then(function(result) {
         // Access the Vega view instance
         //(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 });
 bronzeButtonRef.addEventListener('click', function() {
-    vegaEmbed("#map", bronze_map).then(function(result) {
+    vegaEmbed("#map", bronze_map, {"actions": false}).then(function(result) {
         // Access the Vega view instance
         //(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
 });
 totalButtonRef.addEventListener('click', function() {
-    vegaEmbed("#map", total_map).then(function(result) {
+    vegaEmbed("#map", total_map, {"actions": false}).then(function(result) {
         // Access the Vega view instance
         //(https://vega.github.io/vega/docs/api/view/) as result.view
     }).catch(console.error);
